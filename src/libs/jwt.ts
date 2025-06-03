4412534506869b584ca48d6ca2f6443f78d61690
edcabralc <cabralnet@gmail.com>
2025-06-03 15:07:23 -0300

    feat: add jwt lib

---
+++ src/libs/jwt.ts
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+
+const createJWT = (payload: any) =>
+  jwt.sign(payload, process.env.JWT_SECRET as string);
+
+const readJWT = (token: string) => {
+  try {
+    return jwt.verify(token, process.env.JWT_SECRET as string);
+  } catch (error) {
+    return false;
+  }
+};
+
+export { createJWT, readJWT };
