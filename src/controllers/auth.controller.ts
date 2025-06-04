8395f44529b8bbb5cd088ac285ecb97cfab6d5bf
edcabralc <cabralnet@gmail.com>
2025-06-04 14:22:40 -0300

    feat: auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,24 @@
-const authController = {
-  signup: () => {},
-  signin: () => {},
-  validate: () => {},
+import { RequestHandler } from "express";
+
+import { userValidator } from "@validators/user.validator";
+
+const authController: { [keys: string]: RequestHandler } = {
+  signup: async (req, res) => {
+    const validationResult = userValidator.create(req.body);
+
+    if (!validationResult.success) {
+      res
+        .status(400)
+        .json({ erro: validationResult.error.flatten().fieldErrors });
+      return;
+    }
+
+    // const existingUser = await userService.
+  },
+
+  signin: (req, res) => {},
+
+  validate: (req, res) => {},
 };
 
 export { authController };
